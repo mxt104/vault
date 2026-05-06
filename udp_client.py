32bfa789f9a78afbc098ac5dcc49972e6ccf0559
Mitesh Rane <miteshrane59@gmail.com>
2026-05-06 18:08:59 +0530

    Delete udp_client.py

--- udp_client.py
+++
@@ -1,5 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto("Hello server this is test for UDP protocol".encode(), ("127.0.0.1", 5000))
-print("Request Sent")
